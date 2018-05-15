09b45c4a784973572b75e974dc4aa82aaf67236f
zhangjunh <zhangjunhaoip@gmail.com>
2018-05-15 19:47:34 +0800

    change log name

--- convert.py
+++ convert.py
@@ -12,7 +12,7 @@ def convert(allpath):
             # 指定logger输出格式
             formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
             # 文件日志
-            file_handler = logging.FileHandler("failed.log")
+            file_handler = logging.FileHandler("test.log")
             file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
             # 控制台日志
             console_handler = logging.StreamHandler(sys.stdout)
